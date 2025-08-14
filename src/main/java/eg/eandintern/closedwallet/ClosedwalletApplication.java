695f6898792069113172a76c09001bda0e5ba70b
MahmoudHegazy-01 <mahmoudhegazyuni@gmail.com>
2025-08-14 16:34:25 +0300

    Initial Project Files

---
+++ src/main/java/eg/eandintern/closedwallet/ClosedwalletApplication.java
@@ -0,0 +1,13 @@
+package eg.eandintern.closedwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosedwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosedwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eg/eandintern/closedwallet/ClosedwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package eg.eandintern.closedwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosedwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
